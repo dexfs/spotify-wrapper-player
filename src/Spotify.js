c06b393977bfdc7cc069027ce1b846924fc631e2
André Santos <andre@dexdev.com.br>
2017-10-02 21:20:26 -0300

    finish spotify wrapper player

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQAGyo2XVpEadY7vsW27FMAGIKzWp2dG60_cYkP_WLxKnQXNq_7xdjAyJUppJGY7Vy4lDvjouZd9DeDVofKFYivTUnfJh2LRzft39d-gYjsTN4_BJDv2Iv9O3VHOh9s_aBEadKzl',
+});
+
+export default spotify;
